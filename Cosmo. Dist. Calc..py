f2ab88f6a0e18e6f5d47552e066afa91f9771bbd
MattBzowyckyj <42244332+MattBzowyckyj@users.noreply.github.com>
2018-08-09 10:34:12 -0400

    Delete Cosmo. Dist. Calc..py

--- Cosmo. Dist. Calc..py
+++
@@ -1,6 +0,0 @@
-print('''
-               Welcome to the Cosmological Distance Calculator!''')
-unit = "Mpc"
-z = eval(input("Type in Redshift: "))
-distance = z * (300000/74)
-print( str(distance) + " in " + unit)
